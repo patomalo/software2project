086520ea92ab1aadbf03f88b23ca0fdcb8e57e43
DarK LorD <lordvictor6393@gmail.com>
2014-12-03 08:02:32 -0400

    Adding camera handler

--- js/index.js
+++ js/index.js
@@ -1,3 +1,63 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        pictureSource=navigator.camera.PictureSourceType; 
+        destinationType=navigator.camera.DestinationType;
+        FastClick.attach(document.body);
+        // app.receivedEvent('deviceready');
+    }
+};
+
+window.onload=function(){
+    document.getElementById("foto").style.width = (window.innerWidth-100)+"px";
+    document.getElementById("foto").style.height = (window.innerWidth-100)+"px";
+    document.getElementById("foto").style.backgroundImage="url('img/cordova.png')";
+    document.getElementById("foto").style.backgroundSize="50% 50%";
+};
+
+function capturePhoto() { 
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 90, 
+        destinationType: Camera.DestinationType.DATA_URL, 
+        correctOrientation: true, 
+        targetWidth: 1000, 
+        targetHeight: 1000 
+    }); 
+}
+
+function getPhoto(source) { 
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 90, 
+        destinationType: destinationType.FILE_URI, 
+    sourceType: source }); 
+}
+
+function onSuccess(imageData) { 
+    document.getElementById("foto").style.backgroundImage="url('data:image/jpeg;base64,"+imageData+"')"; 
+    document.getElementById("foto").style.backgroundSize="100% 100%"; 
+} 
+
+function onPhotoURISuccess(imageURI) { 
+    document.getElementById("foto").style.backgroundImage="url('"+imageURI+"')"; 
+    document.getElementById("foto").style.backgroundSize="100% 100%"; 
+}
+
+function onFail(message) { 
+    alert('Failed because: ' + message); 
+}
+
 function viewProduct(id) {
 	window.location='view/product.html';
 }
